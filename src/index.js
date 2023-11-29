584eff3aad5854fa241a4eda7747b64a73e1f357
Annie <colberta31@gmail.com>
2023-11-29 13:44:39 +0000

    Changes to CSS
    moving positioning in CSS
    Changes to classes in JS

--- src/index.js
+++ src/index.js
@@ -81,9 +81,9 @@ function displayForecast(response) {
         `
    <div class="weather-forecast-day">
           <div class="weather-forecast-date">${formatWeekDay(day.time)}</div>
-          <div class="weather-forecast-icon"> <img src="${
-            day.condition.icon_url
-          }" /> </div>
+
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon"/> 
+          
           <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
                 <strong>${Math.round(day.temperature.maximum)}º</strong>
